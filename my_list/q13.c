32285c47570199a6df9e605938875428d6579616
higor21 <felype.cet.15@outlook.com>
2019-06-05 23:23:24 -0300

    question q13 ready

--- my_list/q13.c
+++ my_list/q13.c
@@ -5,15 +5,17 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-void recv_sz_and_dist(int *n, int *local_n, int my_rank, int comm_sz);
-void recv_ipts_and_dist(double local_v1[], double local_v2[], int local_n, int n, double *scalar, int my_rank);
+void recv_sz_and_config(int *n, int *displs, int *sendcounts, int my_rank, int comm_sz);
+void recv_ipts_and_dist(double local_v1[], double local_v2[], int local_n, int n, int *displs,
+                           int *sendcounts, int my_rank);
 double vect_dot_product(double local_v1[], double local_v2[], int local_n);
-void scalar_prod(double local_v1[], double local_v3[], int local_n, double scalar);
-void send_results_and_print(double local_v3[], int local_n, int n, double local_result, int my_rank);
+void vect_sum(double local_v1[], double local_v2[], double local_v3[], int local_n);
+void send_results_and_print(double local_v3[], int local_n, int n, double local_result,
+                              int *displs, int *sendcounts, int my_rank);
 
 int main(void) {
    int my_rank, comm_sz, n, local_n;  
-   double scalar, local_result;
+   double local_result;
    double *local_v1 = NULL, *local_v2 = NULL, *local_v3 = NULL;
 
 
@@ -26,19 +28,20 @@ int main(void) {
    // cada valor indica a quantidade de elementos do vetor local do processo
    int *sendcounts = malloc(sizeof(int)*comm_sz);
 
-   recv_sz_and_dist(&n, &local_n, my_rank, comm_sz);
+   recv_sz_and_config(&n, displs, sendcounts, my_rank, comm_sz);
+
+   local_n = sendcounts[my_rank];
 
    local_v1 = malloc(local_n*sizeof(double));
    local_v2 = malloc(local_n*sizeof(double));
    local_v3 = malloc(local_n*sizeof(double));
 
-   recv_ipts_and_dist(local_v1, local_v2, local_n, n, &scalar, my_rank);
+   recv_ipts_and_dist(local_v1, local_v2, local_n, n, displs, sendcounts, my_rank);
 
    local_result = vect_dot_product(local_v1, local_v2, local_n);
+   vect_sum(local_v1, local_v2, local_v3, local_n);
 
-   scalar_prod(local_v1, local_v3, local_n, scalar);
-
-   send_results_and_print(local_v3, local_n, n, local_result, my_rank);
+   send_results_and_print(local_v3, local_n, n, local_result, displs, sendcounts, my_rank);
 
    free(local_v1);
    free(local_v2);
@@ -48,17 +51,29 @@ int main(void) {
    return 0;
 } 
 
-void recv_sz_and_dist(int *n, int *local_n, int my_rank, int comm_sz){
+void recv_sz_and_config(int *n, int *displs, int *sendcounts, int my_rank, int comm_sz){
    if(my_rank == 0){
       printf("Entre com o tamanhos dos vetores (n):\n");
       scanf("%d", n);
-      *local_n = *n/comm_sz; 
+
+      int rest = *n%comm_sz;
+
+      for (int i = 0; i < comm_sz; i++){
+         sendcounts[i] = *n/comm_sz;
+         if(rest > 0){
+            sendcounts[i]++;
+            rest--;
+         }
+         displs[i] = (i == 0) ? 0 : sendcounts[i-1];
+      }
    }
-   MPI_Bcast(local_n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+   
+   MPI_Bcast(displs, comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
+   MPI_Bcast(sendcounts, comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
 }
 
-void recv_ipts_and_dist(double local_v1[], double local_v2[], int local_n,
-                                    int n, double *scalar, int my_rank){
+void recv_ipts_and_dist(double local_v1[], double local_v2[], int local_n, int n, int *displs,
+                           int *sendcounts, int my_rank){
    double* v1 = NULL;
    double* v2 = NULL;
    if(my_rank == 0){
@@ -72,21 +87,17 @@ void recv_ipts_and_dist(double local_v1[], double local_v2[], int local_n,
       for(int i=0; i<n; i++){
          scanf("%lf", &v2[i]);
       }
-      printf("Entre com o escalar (scalar):\n");
-      scanf("%lf", scalar);
    }
    
-   MPI_Scatter(v1, local_n, MPI_DOUBLE, local_v1, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatterv(v1, sendcounts, displs, MPI_DOUBLE, local_v1, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
    free(v1);
-   MPI_Scatter(v2, local_n, MPI_DOUBLE, local_v2, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatterv(v2, sendcounts, displs, MPI_DOUBLE, local_v2, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
    free(v2);
-   MPI_Bcast(scalar, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 }
 
-void scalar_prod(double local_v1[], double local_v3[], int local_n,
-                           double scalar){
+void vect_sum(double local_v1[], double local_v2[], double local_v3[], int local_n){
    for(int i=0; i<local_n; i++){
-      local_v3[i] = local_v1[i]*scalar;
+      local_v3[i] = local_v1[i] + local_v2[i];
    }
 }
 
@@ -99,14 +110,14 @@ double vect_dot_product(double local_v1[], double local_v2[], int local_n){
 }
 
 void send_results_and_print(double local_v3[], int local_n, int n, double local_result,
-                              int my_rank){
+                              int *displs, int *sendcounts, int my_rank){
    double result;
    double* v3 = NULL;
    MPI_Reduce(&local_result, &result, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
    if(my_rank == 0){
       v3 = malloc(n*sizeof(double));
-      MPI_Gather(local_v3, local_n, MPI_DOUBLE, v3, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-      printf("A multiplicação de v1 pelo escalar gerou: \n");
+      MPI_Gatherv(local_v3, local_n, MPI_DOUBLE, v3, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+      printf("A soma vetorial gerou: \n");
       for(int i=0; i<n; i++){
          printf("%lf ", v3[i]);
       }
@@ -114,7 +125,7 @@ void send_results_and_print(double local_v3[], int local_n, int n, double local_
       printf("\nO produto escalar de v1 com v2 foi: %lf\n", result);
    }
    else{
-      MPI_Gather(local_v3, local_n, MPI_DOUBLE, v3, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+      MPI_Gatherv(local_v3, local_n, MPI_DOUBLE, v3, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
    }
 }
 
